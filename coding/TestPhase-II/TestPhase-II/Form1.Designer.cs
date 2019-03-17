538e85cdb0f7dfe48339506e446de3a023a5a88e
Rana Mateen <a.mateen858@hotmail.com>
2019-03-17 20:36:19 +0500

    Code + Db

---
+++ coding/TestPhase-II/TestPhase-II/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace TestPhase_II
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Searchbar = new System.Windows.Forms.TextBox();
+            this.Searchbutton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.Empid = new System.Windows.Forms.TextBox();
+            this.Empname = new System.Windows.Forms.TextBox();
+            this.Address = new System.Windows.Forms.TextBox();
+            this.DOB = new System.Windows.Forms.TextBox();
+            this.Salary = new System.Windows.Forms.TextBox();
+            this.Timein = new System.Windows.Forms.TextBox();
+            this.Addbutton = new System.Windows.Forms.Button();
+            this.Updatebutton = new System.Windows.Forms.Button();
+            this.Deletebutton = new System.Windows.Forms.Button();
+            this.Clearbutton = new System.Windows.Forms.Button();
+            this.Statusbar = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Teal;
+            this.label1.Font = new System.Drawing.Font("Engravers MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(435, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Employee Form";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(13, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Search by Employee ID";
+            // 
+            // Searchbar
+            // 
+            this.Searchbar.Location = new System.Drawing.Point(169, 50);
+            this.Searchbar.Name = "Searchbar";
+            this.Searchbar.Size = new System.Drawing.Size(157, 20);
+            this.Searchbar.TabIndex = 2;
+            // 
+            // Searchbutton
+            // 
+            this.Searchbutton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.Searchbutton.Font = new System.Drawing.Font("Euclid", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Searchbutton.Location = new System.Drawing.Point(332, 39);
+            this.Searchbutton.Name = "Searchbutton";
+            this.Searchbutton.Size = new System.Drawing.Size(75, 43);
+            this.Searchbutton.TabIndex = 3;
+            this.Searchbutton.Text = "Search";
+            this.Searchbutton.UseVisualStyleBackColor = false;
+            this.Searchbutton.Click += new System.EventHandler(this.Searchbutton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(18, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Emp_ID";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(18, 171);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(16, 201);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Address";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label6.Location = new System.Drawing.Point(16, 232);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 16);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Date of Birth";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label7.Location = new System.Drawing.Point(16, 258);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 16);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Salary";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label8.Location = new System.Drawing.Point(16, 284);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 16);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Time-In";
+            // 
+            // Empid
+            // 
+            this.Empid.Location = new System.Drawing.Point(169, 136);
+            this.Empid.Name = "Empid";
+            this.Empid.Size = new System.Drawing.Size(238, 20);
+            this.Empid.TabIndex = 10;
+            // 
+            // Empname
+            // 
+            this.Empname.Location = new System.Drawing.Point(169, 167);
+            this.Empname.Name = "Empname";
+            this.Empname.Size = new System.Drawing.Size(238, 20);
+            this.Empname.TabIndex = 11;
+            // 
+            // Address
+            // 
+            this.Address.Location = new System.Drawing.Point(169, 197);
+            this.Address.Name = "Address";
+            this.Address.Size = new System.Drawing.Size(238, 20);
+            this.Address.TabIndex = 12;
+            // 
+            // DOB
+            // 
+            this.DOB.Location = new System.Drawing.Point(169, 228);
+            this.DOB.Name = "DOB";
+            this.DOB.Size = new System.Drawing.Size(238, 20);
+            this.DOB.TabIndex = 13;
+            // 
+            // Salary
+            // 
+            this.Salary.Location = new System.Drawing.Point(169, 257);
+            this.Salary.Name = "Salary";
+            this.Salary.Size = new System.Drawing.Size(238, 20);
+            this.Salary.TabIndex = 14;
+            // 
+            // Timein
+            // 
+            this.Timein.Location = new System.Drawing.Point(169, 284);
+            this.Timein.Name = "Timein";
+            this.Timein.Size = new System.Drawing.Size(238, 20);
+            this.Timein.TabIndex = 15;
+            // 
+            // Addbutton
+            // 
+            this.Addbutton.Location = new System.Drawing.Point(19, 323);
+            this.Addbutton.Name = "Addbutton";
+            this.Addbutton.Size = new System.Drawing.Size(56, 23);
+            this.Addbutton.TabIndex = 16;
+            this.Addbutton.Text = "Add";
+            this.Addbutton.UseVisualStyleBackColor = true;
+            this.Addbutton.Click += new System.EventHandler(this.Addbutton_Click);
+            // 
+            // Updatebutton
+            // 
+            this.Updatebutton.Location = new System.Drawing.Point(81, 323);
+            this.Updatebutton.Name = "Updatebutton";
+            this.Updatebutton.Size = new System.Drawing.Size(51, 23);
+            this.Updatebutton.TabIndex = 17;
+            this.Updatebutton.Text = "Update";
+            this.Updatebutton.UseVisualStyleBackColor = true;
+            this.Updatebutton.Click += new System.EventHandler(this.Updatebutton_Click);
+            // 
+            // Deletebutton
+            // 
+            this.Deletebutton.Location = new System.Drawing.Point(138, 323);
+            this.Deletebutton.Name = "Deletebutton";
+            this.Deletebutton.Size = new System.Drawing.Size(54, 23);
+            this.Deletebutton.TabIndex = 18;
+            this.Deletebutton.Text = "Delete";
+            this.Deletebutton.UseVisualStyleBackColor = true;
+            this.Deletebutton.Click += new System.EventHandler(this.Deletebutton_Click);
+            // 
+            // Clearbutton
+            // 
+            this.Clearbutton.Location = new System.Drawing.Point(199, 323);
+            this.Clearbutton.Name = "Clearbutton";
+            this.Clearbutton.Size = new System.Drawing.Size(57, 23);
+            this.Clearbutton.TabIndex = 19;
+            this.Clearbutton.Text = "Clear";
+            this.Clearbutton.UseVisualStyleBackColor = true;
+            this.Clearbutton.Click += new System.EventHandler(this.Clearbutton_Click);
+            // 
+            // Statusbar
+            // 
+            this.Statusbar.BackColor = System.Drawing.Color.Black;
+            this.Statusbar.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Statusbar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Statusbar.ForeColor = System.Drawing.Color.Black;
+            this.Statusbar.Location = new System.Drawing.Point(21, 88);
+            this.Statusbar.Name = "Statusbar";
+            this.Statusbar.Size = new System.Drawing.Size(386, 15);
+            this.Statusbar.TabIndex = 21;
+            this.Statusbar.Text = "Status";
+            this.Statusbar.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(459, 358);
+            this.Controls.Add(this.Statusbar);
+            this.Controls.Add(this.Clearbutton);
+            this.Controls.Add(this.Deletebutton);
+            this.Controls.Add(this.Updatebutton);
+            this.Controls.Add(this.Addbutton);
+            this.Controls.Add(this.Timein);
+            this.Controls.Add(this.Salary);
+            this.Controls.Add(this.DOB);
+            this.Controls.Add(this.Address);
+            this.Controls.Add(this.Empname);
+            this.Controls.Add(this.Empid);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Searchbutton);
+            this.Controls.Add(this.Searchbar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "TestPhase-II";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Searchbar;
+        private System.Windows.Forms.Button Searchbutton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox Empid;
+        private System.Windows.Forms.TextBox Empname;
+        private System.Windows.Forms.TextBox Address;
+        private System.Windows.Forms.TextBox DOB;
+        private System.Windows.Forms.TextBox Salary;
+        private System.Windows.Forms.TextBox Timein;
+        private System.Windows.Forms.Button Addbutton;
+        private System.Windows.Forms.Button Updatebutton;
+        private System.Windows.Forms.Button Deletebutton;
+        private System.Windows.Forms.Button Clearbutton;
+        private System.Windows.Forms.TextBox Statusbar;
+    }
+}
+
---
+++ coding/TestPhase-II/TestPhase-II/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Odbc;
+using System.Data.SqlClient;
+
+namespace TestPhase_II
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+                    private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Searchbutton_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source = RANA-MATEEN\SQLEXPRESS; Initial Catalog = CRUD_DB; Integrated Security = True");
+            SqlDataAdapter sqd = new SqlDataAdapter("SELECT * FROM Employee where Emp_Id ='" + Searchbar.Text + "' ", con);
+            DataTable dt = new DataTable();
+            sqd.Fill(dt);
+            if (dt.Rows.Count>0)
+            {
+                Statusbar.Text = "Record Found";
+                Statusbar.ForeColor = Color.Green;
+                Statusbar.Font = new Font(Statusbar.Font, FontStyle.Bold);
+
+                
+
+                Empid.Text = dt.Rows[0][0].ToString();
+                Empname.Text = dt.Rows[0][1].ToString();
+                Address.Text = dt.Rows[0][2].ToString();
+                DOB.Text = dt.Rows[0][3].ToString();
+                Salary.Text = dt.Rows[0][4].ToString();
+                Timein.Text = dt.Rows[0][5].ToString();
+            }
+            else
+            {
+                Statusbar.Text = "Record NOT Found";
+                Statusbar.ForeColor = Color.Red;
+                Empid.Text = "";
+                Empname.Text = "";
+                Address.Text = "";
+                DOB.Text = "";
+                Salary.Text = "";
+                Timein.Text = "";
+            }
+        }
+
+        private void Addbutton_Click(object sender, EventArgs e)
+        {
+            
+                SqlConnection con = new SqlConnection(@"Data Source = RANA-MATEEN\SQLEXPRESS; Initial Catalog = CRUD_DB; Integrated Security = True");
+                con.Open();
+
+                SqlCommand sqc = new SqlCommand("INSERT INTO Employee (Emp_Id, Name, Address, DOB, Salary, Time_In) VALUES ('" + Empid.Text + "', '" + Empname.Text + "','" + Address.Text + "','" + DOB.Text.ToString() + "','" + Salary.Text + "','" + Timein.Text + "')", con);
+                int a = sqc.ExecuteNonQuery();
+            if (a > 0)
+            {
+                Statusbar.Text = "Record Inserted Successfully";
+                Statusbar.ForeColor = Color.Green;
+                Statusbar.Font = new Font(Statusbar.Font, FontStyle.Bold);
+                con.Close();
+            }
+            else {
+                Statusbar.Text = "Problem Occurred! Try Again";
+                Statusbar.ForeColor = Color.Red;
+                Statusbar.Font = new Font (Statusbar.Font, FontStyle.Bold ) ;
+            }
+            //MessageBox.Show("Record Inserted");
+                
+            
+            
+        }
+
+        private void Updatebutton_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source = RANA-MATEEN\SQLEXPRESS; Initial Catalog = CRUD_DB; Integrated Security = True");
+            con.Open();
+
+            SqlCommand sqc = new SqlCommand("update Employee set Emp_Id = '"+ Empid.Text +"', Name='"+Empname.Text+"', Address='"+Address.Text+"', DOB='"+DOB.Text.ToString()+"', Salary = '"+Salary.Text+"', Time_In = '"+Timein.Text.ToString()+"' where Emp_Id = '"+Empid.Text+"'", con);
+            int a = sqc.ExecuteNonQuery();
+            if (a > 0)
+            {
+                Statusbar.Text = "Record Updated Successfully";
+                Statusbar.ForeColor = Color.Green;
+                Statusbar.Font = new Font(Statusbar.Font, FontStyle.Bold);
+                con.Close();
+            }
+            else
+            {
+                Statusbar.Text = "Problem Occurred! Try Again";
+                Statusbar.ForeColor = Color.Red;
+                Statusbar.Font = new Font(Statusbar.Font, FontStyle.Bold);
+                con.Close();
+            }
+            //MessageBox.Show("Record Updated");
+            
+        }
+
+        private void Deletebutton_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source = RANA-MATEEN\SQLEXPRESS; Initial Catalog = CRUD_DB; Integrated Security = True");
+            con.Open();
+
+            SqlCommand sqc = new SqlCommand("DELETE FROM EMPLOYEE where Emp_Id = '" + Empid.Text + "'", con);
+            int a = sqc.ExecuteNonQuery();
+            if (a > 0)
+            {
+                Statusbar.Text = "Record Deleted";
+                Statusbar.ForeColor = Color.Green;
+                Statusbar.Font = new Font(Statusbar.Font, FontStyle.Bold);
+                Empid.Text = "";
+                Empname.Text = "";
+                Address.Text = "";
+                DOB.Text = "";
+                Salary.Text = "";
+                Timein.Text = "";
+                con.Close();
+            }
+            else
+            {
+                Statusbar.Text = "Problem Occurred! Try Again";
+                Statusbar.ForeColor = Color.Red;
+                Statusbar.Font = new Font(Statusbar.Font, FontStyle.Bold);
+                con.Close();
+            }
+            //MessageBox.Show("Record Deleted");
+            //con.Close();
+        }
+
+        private void Clearbutton_Click(object sender, EventArgs e)
+        {
+            Statusbar.Text = "";
+            Empid.Text = "";
+            Empname.Text = "";
+            Address.Text = "";
+            DOB.Text = "";
+            Salary.Text = "";
+            Timein.Text = "";
+            Searchbar.Text = "";
+        }
+    }
+}
+
